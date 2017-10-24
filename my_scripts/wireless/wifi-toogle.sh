164d1cb2bb86c9071e28bfe86923a5e894f12fe8
flaz14 <flazfourteen@gmail.com>
2017-10-24 19:53:42 +0300

    Rework script that toogles WiFi with aid of Network Manager CLI only.

--- my_scripts/wireless/wifi-toogle.sh
+++ my_scripts/wireless/wifi-toogle.sh
@@ -1,24 +1,49 @@
 #!/bin/bash
 
+###
+# This script helps to toogle WiFi on the fucking Acer Aspire-ES1-521 laptop (because inborn combination of Fn + F3 keys
+# doesn't work properly in Linux).
+#
+# It's possible to use `rfkill` command here. But `rfkill` doesn't get on with Gnome Network Manager. You can find more 
+# information about race conditions between `rfkill` and Network Manager in the thread:
+# [How can I keep a wireless card's radio powered off by default?]
+# (https://askubuntu.com/questions/24171/how-can-i-keep-a-wireless-cards-radio-powered-off-by-default)
+#
+# So we use only possibilities of Network Manager, e.g. its command line interface here and evewhere when deal with 
+# WiFi.
+###
 
-overall_wifi_status=`rfkill list wifi`
+# Below is the sample output (e.g. copied'n'pasted from a terminal emulator) of `nmcli -fields WIFI nm status` command:
+#WIFI      
+#disabled  
 
-total_lines_in_status=`echo "$overall_wifi_status" | wc --lines`
+nm_status="$(nmcli -fields WIFI nm status)"
 
-if (( $total_lines_in_status != 3 )); then
-	error_message=$"Cannot determine WiFi status due to unexpected output from \`rfkill\`:\n$overall_wifi_status"
+total_lines_in_status="$(echo "$nm_status" | wc --lines)"
+
+if (( $total_lines_in_status != 2 )); then
+	error_message=$"Cannot determine WiFi status due to unexpected output from \`nmcli -fields WIFI nm status\`:\n$nm_status"
 	notify-send 'Ugly Tricks' "$error_message"
+	exit 1
 fi
 
-soft_blocked_line=`echo "$overall_wifi_status" | grep 'Soft blocked:'`
+table_heading="$(echo "$nm_status" | head --lines=1)"
 
-total_lines_in_soft_blocked_line=`echo "$soft_blocked_line" | wc --lines`
+table_content="$(echo "$nm_status" | tail --lines=1)"
 
-if (( $total_lines_in_soft_blocked_line !=1 )); then
-	error_message=$"Soft blocked status is absent from \`rfkill\`:\n$overall_wifi_status"
+# The output of `nmcli status` command is a table. Items in the table are arranged with aid of spaces. For robust 
+# verifying of WiFi's status we need to eliminate extra spaces. `xargs` helps us to achieve this (as well as remove 
+# whitespaces in the middle of the string; however, that doesn't matter in our script).
+#
+# You can find more information about this trick in the answer of @makevoid:
+# [How to trim whitespace from a Bash variable?](https://stackoverflow.com/a/12973694/4672928)
+wifi_status="$(echo "$table_content" | xargs)"
+
+if [ "$wifi_status" == 'disabled' ]; then
+	wifi-turn-on.sh
+elif [ "$wifi_status" == 'enabled' ]; then
+	wifi-turn-off.sh
+else
+	error_message=$"Cannot determine WiFi status. Expected 'enabled' or 'disabled' but '$wifi_status' received."
 	notify-send 'Ugly Tricks' "$error_message"
 fi
-
-
-wifi_status_regexp='Soft blocked: (yes|no)'
-soft_blocked_line =~ $wifi_status_regexp
